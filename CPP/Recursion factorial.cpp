92a06763474ab3fc65cec66513126e28192211a2
Gaurav Rana <78133596+gaurav1253@users.noreply.github.com>
2023-01-18 23:16:11 +0530

    Create Recursion factorial.cpp

---
+++ CPP/Recursion factorial.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int factorial(int n)
+{
+  if(n>1){
+    return n*factorial(n-1);
+  }
+  else{
+    return 1;
+  }
+
+}
+int main()
+{
+  int n;
+  cin>>n;
+  int result = factorial(n);
+  cout<<result;
+  return 0;
+}
+ 
+
+ 
